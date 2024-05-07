d55da41db23c3814d1175ef907a562bd7918153a
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-07 11:04:40 +0200

    Add interactivity to edit button

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,11 +1,26 @@
+// NPM Packages
+import { useState } from 'react';
+
 export default function Players({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  console.log('isEditing:', isEditing);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required placeholder="Enter name" />;
+  }
+
   return (
     <li>
-      <span className="player-info">
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
