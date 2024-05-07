23398f9e42a1646d1f9e2e154b40b15b5a3637a1
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-07 12:30:47 +0200

    Add playerName state & two-way binding to user name input

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Players name="Player 1" symbol="X" />
-          <Players name="Player 2" symbol="O" />
+          <Players initialName="Player 1" symbol="X" />
+          <Players initialName="Player 2" symbol="O" />
         </ol>
         <div>Game Board</div>
       </div>
