4051c98394d5691d934d099a4996bafbf20f92e1
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-07 10:42:51 +0200

    Refactor: create Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Players from './components/Players';
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Players name="Player 1" symbol="X" />
+          <Players name="Player 2" symbol="O" />
         </ol>
         <div>Game Board</div>
       </div>
