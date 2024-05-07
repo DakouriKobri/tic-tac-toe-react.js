07861b25baa96f4857f7bcf6d7a168e8685df69e
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-07 09:06:34 +0200

    Add initial app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <h2 style={{ color: 'brown' }}>Coming...</h2>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        <div>Game Board</div>
+      </div>
+
+      <div>Game Log</div>
+    </main>
+  );
 }
 
 export default App;
