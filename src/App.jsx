931b7b2e75a87606cf2c0026f1e20a64672f37ad
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-09 22:25:43 +0200

    Add functionality to reset game

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   let activePlayer = derivedActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard].map((array) => [...array]);
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -74,6 +74,10 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -90,7 +94,9 @@ function App() {
           />
         </ol>
 
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
 
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
