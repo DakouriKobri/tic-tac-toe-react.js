f0411aaf21ff042b610d5397941ca5dacf16cd07
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-11 16:15:52 +0200

    Extract constant and helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,25 @@ import { useState } from 'react';
 
 // Project Imports
 import GameBoard from './components/GameBoard';
-import Players from './components/Players';
-import Log from './components/Log';
-import { WINNING_COMBINATIONS } from './winning-combinations';
 import GameOver from './components/GameOver';
+import Log from './components/Log';
+import Players from './components/Players';
+import { derivedActivePlayer, derivedGameBoard, derivedWinner } from './utils';
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-function derivedActivePlayer(gameTurns) {
-  let currentPlayer = 'X';
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O';
-  }
-
-  return currentPlayer;
-}
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
 
 function App() {
-  const [players, setPlayers] = useState({ X: 'Player 1', O: 'Player 2' });
+  const [players, setPlayers] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
 
   let activePlayer = derivedActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard].map((array) => [...array]);
+  const gameBoard = derivedGameBoard(gameTurns);
 
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, column } = square;
-
-    gameBoard[row][column] = player;
-  }
-
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
-  }
+  const winner = derivedWinner(gameBoard, players);
 
   const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -93,13 +59,13 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Players
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === 'X'}
             onChangeName={handlePlayerNameChange}
           />
           <Players
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === 'O'}
             onChangeName={handlePlayerNameChange}
---
+++ src/utils.js
@@ -0,0 +1,53 @@
+// Project Imports
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export function derivedGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD].map((array) => [...array]);
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, column } = square;
+
+    gameBoard[row][column] = player;
+  }
+
+  return gameBoard;
+}
+
+export function derivedActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+export function derivedWinner(gameBoard, players) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+
+  return winner;
+}
